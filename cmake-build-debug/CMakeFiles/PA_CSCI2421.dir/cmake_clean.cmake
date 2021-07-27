adf967d15562a7b01bdadcc6b8aca72af1758d1d
Matthew Starkey <matthwe.starkey@outlook.com>
2021-07-27 12:57:00 -0600

    added search function

---
+++ cmake-build-debug/CMakeFiles/PA_CSCI2421.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PA_CSCI2421.dir/main.cpp.o"
+  "PA_CSCI2421"
+  "PA_CSCI2421.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PA_CSCI2421.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
